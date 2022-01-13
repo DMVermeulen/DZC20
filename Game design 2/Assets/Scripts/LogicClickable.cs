fefd17e4ef6bde23a6ff37419860ac31a7225377
Davey Vermeulen <d.m.vermeulen@student.tue.nl>
2022-01-13 17:42:24 +0100

    Logic update 2
    - Moved explanation to top board
    - Added 1 (unsolvable) exercise
    - Implemented LogicClickable: exercises now light up when hovered and are clickable to cycle through their states (?, 0, 1).

---
+++ Game design 2/Assets/Scripts/LogicClickable.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LogicClickable : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    [Tooltip("The colour to use when highlighting this object.")]
+    [SerializeField] private Color colourHighlight = new Color(1, 1, 0, 1);
+
+    [Tooltip("The base colour of this object.")]
+    [SerializeField] private Color colourBase = Color.white;
+
+    private TextMesh text;
+    private int state = -1;
+
+    // Called once at startup 
+    public void Start()
+    {
+        // Retrieve text child
+        text = GetComponentInChildren<TextMesh>();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // Loop through the possible states
+        state++;
+
+        // Wraparound
+        if (state > 1) state = -1;
+
+        // Set text to new state
+        text.text = (state >= 0) ? $"{state}" : "?";
+
+        Debug.Log("Clicked on " + name + " GameObject");
+    }
+
+    // Change colour to yellow-ish when hovering over object
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text.color = colourHighlight;
+        Debug.Log("Cursor entering " + name + " GameObject");
+    }
+
+    // Return colour to basic state
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.color = colourBase;
+        Debug.Log("Cursor exiting " + name + " GameObject");
+    }
+}
---
+++ Game design 2/Assets/Scripts/ObjectDrag.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ObjectDrag : MonoBehaviour
+{
+	private Vector3 screenPoint;
+	private Vector3 offset;
+
+	// Calculate offset compared to object
+	public void OnMouseDown()
+	{
+		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+	}
+
+	public void OnMouseDrag()
+	{
+		Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
+		transform.position = cursorPosition;
+	}
+}
