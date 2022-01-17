7f9cc2c6e5d4eaab874aff71af3c2d12903ff74b
SJacobs501 <simonjacobs501@gmail.com>
2022-01-17 22:11:59 +0100

    Add checking if code is correct

---
+++ Game design 2/Assets/Scripts/safe/EncryptedMessage.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EncryptedMessage : MonoBehaviour
+{
+    public Text key;
+    public Text message;
+    public Text encryptedMessage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("started encrypted message");
+        print(key.text);
+        print(message.text);
+        print(CaesarShiftString(message.text, int.Parse(key.text)));
+        encryptedMessage.text = CaesarShiftString(message.text, int.Parse(key.text));
+    }
+
+    // Shifts a string using the caesar cipher
+    public string CaesarShiftString(string value, int shift)
+    {
+        char[] buffer = value.ToCharArray();
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            // Letter.
+            char letter = buffer[i];
+            // Add shift to all.
+            letter = (char)(letter + shift);
+            // Subtract 26 on overflow.
+            // Add 26 on underflow.
+            if (letter > 'z')
+            {
+                letter = (char)(letter - 26);
+            }
+            else if (letter < 'a')
+            {
+                letter = (char)(letter + 26);
+            }
+            // Store.
+            buffer[i] = letter;
+        }
+        return new string(buffer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Game design 2/Assets/Scripts/safe/Submit.cs
+++ Game design 2/Assets/Scripts/safe/Submit.cs
@@ -8,42 +8,19 @@ public class Submit : MonoBehaviour
 {
     public Text result;
     public Text key;
+    public Text message;
     public Text encryptedMessage;
     public InputField inputField;
 
     public void SubmitPuzzle() {
-        result.text = "Input: " + inputField.text + ", key: " + key.text + " encrypted: " + encryptedMessage.text + " solution: " + Solution();
-    }
-
-    public string Solution() {
-        int shift = int.Parse(key.text);
-        string result = CaesarShiftString(encryptedMessage.text, -shift);
-        return result;
-    }
-
-    // Shifts a string using the caesar cipher
-    public string CaesarShiftString(string value, int shift)
-    {
-        char[] buffer = value.ToCharArray();
-        for (int i = 0; i < buffer.Length; i++)
-        {
-            // Letter.
-            char letter = buffer[i];
-            // Add shift to all.
-            letter = (char)(letter + shift);
-            // Subtract 26 on overflow.
-            // Add 26 on underflow.
-            if (letter > 'z')
-            {
-                letter = (char)(letter - 26);
-            }
-            else if (letter < 'a')
-            {
-                letter = (char)(letter + 26);
-            }
-            // Store.
-            buffer[i] = letter;
+        bool correct = Correct();
+        result.text = "Input: " + inputField.text + ", key: " + key.text + " encrypted: " + encryptedMessage.text + " solution: " + message.text + " correct:"+correct;
+        if (correct) {
+            result.color = Color.green;
         }
-        return new string(buffer);
+    }
+
+    public bool Correct() {
+        return message.text == inputField.text;
     }
 }
