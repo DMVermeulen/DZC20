102807454ba00246b571c801fd821e6271b08182
Davey Vermeulen <d.m.vermeulen@student.tue.nl>
2022-01-18 15:46:55 +0100

    Limit framerate in SceneSwitcher
    I got fed up with my GPU melting every time I pressed play

--- Game design 2/Assets/Scripts/SceneSwitcher.cs
+++ Game design 2/Assets/Scripts/SceneSwitcher.cs
@@ -4,6 +4,12 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
+    // Called at first frame
+    public void Start()
+    {
+        // Limit framerate so we don't melt GPUs trying to render 500 frames of Audi 7 in 1 second
+        Application.targetFrameRate = 60;
+    }
     // Load scene at buildIndex parameter
     public void Load(int buildIndex)
     {
